afb7783ef8a5f45a786629cf6a04583027190d25
srv48 <srvb1729@gmail.com>
2017-06-23 18:09:37 +0530

    Jump Added. Keyboard movement code modified

--- Assets/Scripts/Keyboard_Mouse_Controls.cs
+++ Assets/Scripts/Keyboard_Mouse_Controls.cs
@@ -7,17 +7,19 @@ public class Keyboard_Mouse_Controls : MonoBehaviour {
 
 	private float speed = 4f;
 	private CharacterController cc;
-	private float gravity = -9.8f;
+	private Vector3 gravity;
 	private float rotationX = 0.0f; 
 	public float sensitivityHori = 9.0f;
 	public float sensitivityVert = 9.0f;
 	private float minVert = -30.0f, maxVert = 45.0f; 
+	private Vector3 movement = Vector3.zero;
+	private float jumpSpeed = 200.0f;
+	private bool jump = false;
 
 
 	// Use this for initialization
 	void Start () {
 		cc = GetComponent<CharacterController> ();
-
 	}
 	
 	// Update is called once per frame
@@ -36,32 +38,51 @@ public class Keyboard_Mouse_Controls : MonoBehaviour {
 		float horizontal, vertical;														//keyboard movement begins
 		horizontal = Input.GetAxis ("Horizontal") * speed;
 		vertical = Input.GetAxis ("Vertical") * speed;
-		Vector3 movement = new Vector3 (-horizontal, 0, -vertical);
+		movement = new Vector3 (-horizontal, 0f, -vertical);
 
 		movement = Vector3.ClampMagnitude (movement, speed);
         //MouseLook obj = new MouseLook();
-        movement = transform.rotation * movement;
-		movement *= Time.deltaTime;
-		movement.y = gravity;
-		//movement = transform.TransformDirection (movement);
-		cc.Move (movement);																//keyboard movement ends
+        movement = transform.rotation * movement;							//keyboard movement ends
 
 
 
-		if (Input.GetKey (KeyCode.LeftControl))									//crouch code begins
-			cc.transform.position = new Vector3(cc.transform.position.x, .1f, cc.transform.position.z);
+
+
+		//float crouch = cc.transform.position.y;
+		if (Input.GetKey (KeyCode.LeftControl)) {									//crouch code begins
+			cc.height = -5f;
+			//cc.transform.position = new Vector3 (cc.transform.position.x, cc.transform.position.y - 1.5f, cc.transform.position.z);
+			//crouch = cc.transform.position.y + 1.5f;
+		}
 		else
-			cc.transform.position = new Vector3(cc.transform.position.x, 1.58f, cc.transform.position.z);												//crouch code ends
-
+			cc.height = 6f;												//crouch code ends
+			//cc.transform.position = new Vector3(cc.transform.position.x, crouch , cc.transform.position.z);
+		
 
 
 		if (Input.GetKey (KeyCode.LeftShift))									//sprint code begins
 			speed = 8f;
 		else
-			speed = 4f;															//sprint code ends
+			speed = 4f;		 												//sprint code ends
+		
 
-		//if(Input.GetKey(KeyCode.Space))
-			
+		if (Input.GetKey (KeyCode.Space) && cc.isGrounded)					//jumping code begins
+			jump = true;
+
+		if (cc.isGrounded) {
+			gravity.y = 0f;
+			if (jump) {
+				gravity.y = jumpSpeed;
+				jump = false;
+			}
+		}
+		else {
+			gravity = new Vector3 (0f, -300f, 0f) * Time.deltaTime;
+		}																	//jumping code ends
+
+		movement += gravity;
+
+		cc.Move (movement * Time.deltaTime);	
 
 
 	}
